9848bafa599fc9242faa26a491192ffd47ba0d54
Cabbage-Roll <62716710+Cabbage-Roll@users.noreply.github.com>
2020-05-15 14:50:12 +0200

    Update and rename g to main.cpp

--- main.cpp
+++ main.cpp
@@ -49,7 +49,52 @@ public:
 
 };
 
+enum Vel{XS,S,M,L,XL};
+
+enum Tipodece{MAJICA,PANTALONE};
+
 class Odeca: public Proizvod{
+private:
+    Vel velicina;
+    Tipodece tip;
+
+public:/*
+    int izbroj(){
+        int i, n=0;
+        for(i=0;i<size;i++){
+            if(tip==L || tip==XL){
+                n++;
+            }
+        }
+        return n;
+    }*/
+
+    Odeca(Vel v, Tipodece t, DinString a, int b, int c, DinString d){
+        zemljaPorekla=a;
+        rokTrajanja=b;
+        cena=c;
+        naziv=d;
+        velicina=v;
+        tip=t;
+    }
+
+    virtual void izracunajPopust(){
+        if(velicina==XS && tip==MAJICA)
+            cena=(100*cena-60*cena)/100;
+        else if(tip==MAJICA)
+            cena=(100*cena-50*cena)/100;
+        else if(tip==PANTALONE)
+            cena=(100*cena-33*cena)/100;
+    };
+
+    void ispisi(){
+        cout<<"zemlja porekla "<<zemljaPorekla<<endl;
+        cout<<"rok trajanja "<<rokTrajanja<<endl;
+        cout<<"cena "<<cena<<endl;
+        cout<<"naziv "<<naziv<<endl;
+        cout<<"velicina "<<velicina<<endl;
+        cout<<"tip "<<tip<<endl;
+    }
 
 
 };
